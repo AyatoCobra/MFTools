00a3e2ed7b547a80a568eb51179dea8fa26c6410
Bryan Kogfield <85810842+AyatoCobra@users.noreply.github.com>
2026-08-19 21:37:51 +0300

    Add files via upload

--- MFTools.lua
+++ MFTools.lua
@@ -1,12 +1,12 @@
 --[[
-    MFTools (Mordor Faction Tools) v1.6 (beta test)
+    MFTools (Mordor Faction Tools) v1.0 (beta test)
     Главный файл (Ядро + Автоустановщик + Автообновление)
     Разработчик: Bryan Kogfield (Богдан)
 ]]
 
 script_name("MFTools")
 script_author("Bryan Kogfield")
-script_version("1.6 (beta test)")
+script_version("1.0 (beta test)")
 
 require "lib.moonloader"
 local samp = require "lib.samp.events"
@@ -19,8 +19,8 @@ local vk = require "vkeys"
 -- ==========================================
 -- === НАСТРОЙКИ АВТООБНОВЛЕНИЯ И ЗАГРУЗКИ ===
 -- ==========================================
-local SCRIPT_VERSION = 1.6 
-local SCRIPT_VERSION_TEXT = "1.6 (beta test)"
+local SCRIPT_VERSION = 1.0 
+local SCRIPT_VERSION_TEXT = "1.0 (beta test)"
 local UPDATE_JSON_URL = "https://raw.githubusercontent.com/AyatoCobra/MFTools/main/update.json" 
 local MAIN_SCRIPT_URL = "https://raw.githubusercontent.com/AyatoCobra/MFTools/main/MFTools.lua" 
 
